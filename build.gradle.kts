a7c45e723bae572b30dd7c96a6333a3525d7f331
booky10 <boooky10@gmail.com>
2024-05-04 03:44:05 +0200

    Set final plugin as mojang-mapped
    Only uses api methods, remapping this is useless

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,4 +69,10 @@ tasks {
     assemble {
         dependsOn(shadowJar)
     }
+
+    withType<Jar> {
+        manifest.attributes(
+            "paperweight-mappings-namespace" to "mojang"
+        )
+    }
 }
